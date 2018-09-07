8a6b98a83c98f55b72b3dd210c8e4cdc2591cc4e
Vikas Pundora <phpyouth@vikass-macbook-pro.local>
2018-09-07 17:55:45 +0530

    test

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftJsonParsingCodable' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftJsonParsingCodable
+
+  pod 'Alamofire', '~> 4.7'
+
+  target 'SwiftJsonParsingCodableTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SwiftJsonParsingCodableUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- SwiftJsonParsingCodable/ViewController.swift
+++ SwiftJsonParsingCodable/ViewController.swift
@@ -7,19 +7,117 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+/*
+struct Response:Codable{
+    let status:String?
+    let result:[DealData]
+}
 
+struct DealData:Codable{
+    let deal_id: String?
+    let qrcode: String?
+    let description: String?
+}
+*/
+
+struct BusinessDetail:Codable{
+    let status:String?
+    let result:[BusinessData]
+}
+// Business Detail
+
+struct BusinessData:Codable{
+    let name: String?
+    let address: String?
+}
+
+class ViewController: UITableViewController {
+
+    let cellIdentifier = "cell"
+    
+    let API_URL = "https://pukkading.com/mobile_app/deals.php"
+    
+    let App_URL_IOS = "https://pukkading.com/mobile_app/iosapp1.php"
+    
+    var arrayData = [BusinessData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // register a tableview cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.dataSource = self
+        jsonParse() // calling api
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arrayData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        cell?.textLabel?.text = arrayData[indexPath.row].address
+        return cell!
+    }
+    
+    func jsonParse(){
+        
+        let parameters = [
+            "author_id" :"1907",
+            "get_deals_author":"1"
+        ]
+        
+        Alamofire.upload(
+            multipartFormData: { MultipartFormData in
+                
+                for (key, value) in parameters {
+                    
+                    MultipartFormData.append((value as AnyObject).data(using: String.Encoding.utf8.rawValue)!, withName: key)
+                }
+                
+        }, to: App_URL_IOS) { (result) in
+            
+            switch result {
+            case .success(let upload, _, _):
+                
+                upload.responseJSON { response in
+                    
+                    let json = response.data
+                    //print(response.result.value as Any)
+                    do{
+                        //created the json decoder
+                        let decoder = JSONDecoder()
+                        
+                        do {
+                            let status = try decoder.decode(BusinessDetail.self, from: json!)
+                            
+                            print("Status : \(String(describing: status.status))")
+                            print("Result Array : \(status.result)")
+                            
+                            self.arrayData = status.result
+                            //self.arrayData.reverse() // Used to reverse Array
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        } catch let error {
+                            print("Error: \(error)")
+                        }
+                    }
+                }
+            case .failure(let encodingError):
+                print(encodingError)
+                break
+            }
+        }
+    }
 }
 
